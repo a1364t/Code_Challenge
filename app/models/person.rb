6d40fb27df23f4fc273b397a05c987ed2853c537
Alireza <ali1985reza@gmail.com>
2022-08-18 18:15:05 +1000

    first part is working

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
+    validates :first_name, :presence => true, :uniqueness => true
     belongs_to :affiliation, :optional => true
     belongs_to :location, :optional => true
 end
